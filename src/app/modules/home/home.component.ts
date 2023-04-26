e166d165b0a7d8e5bdb2a2834123876b86e504cc
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-04-26 11:50:57 -0400

    display list of goals on home page

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Goal } from './models/goal.model';
+import { GoalService } from './services/goal.service';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  goals: Goal[] = [];
+  showLoginModal = false;
+  showAddGoalForm = false;
 
+  constructor(
+    private goalService: GoalService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.goalService.getGoals().subscribe((goals) => {
+      this.goals = goals;
+    });
+
+    this.authService.user$.subscribe((user) => {
+      console.log(user, 'user changed');
+
+      if (user.username === '') {
+        this.showLoginModal = true;
+      } else {
+        this.showLoginModal = false;
+      }
+    });
+  }
+
+  handleAddGoalClick() {
+    this.showAddGoalForm = true;
+  }
 }
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,9 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
+import { GoalItemComponent } from './components/goal-item/goal-item.component';
+import { GoalListComponent } from './components/goal-list/goal-list.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  declarations: [
+    HomeComponent,
+    ProgressBarComponent,
+    GoalItemComponent,
+    GoalListComponent,
+    LoginFormComponent,
+    ModalComponent,
+  ],
+  imports: [CommonModule, HomeRoutingModule, FormsModule],
 })
 export class HomeModule {}
