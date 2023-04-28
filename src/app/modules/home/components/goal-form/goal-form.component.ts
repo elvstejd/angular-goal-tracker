d5dc48b6a59f1df4023df5742e3dab5a6a0ef4a9
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-04-28 16:25:14 -0400

    show dynamic submit button label, fix date loading

--- src/app/modules/home/components/goal-form/goal-form.component.ts
+++ src/app/modules/home/components/goal-form/goal-form.component.ts
@@ -1,17 +1,30 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Goal } from '../../models/goal.model';
 
 @Component({
   selector: 'app-goal-form',
   templateUrl: './goal-form.component.html',
   styleUrls: ['./goal-form.component.css'],
 })
-export class GoalFormComponent {
+export class GoalFormComponent implements OnInit {
   formData = {
     title: '',
     due_date: new Date().toISOString().substring(0, 10),
     progress: 0,
   };
   @Output() onSubmit = new EventEmitter<typeof this.formData>();
+  @Input() editGoal: Goal | null = null;
+  buttonLabel = 'Add';
+
+  ngOnInit(): void {
+    if (this.editGoal) {
+      this.buttonLabel = 'Edit';
+      this.formData = {
+        ...this.editGoal,
+        due_date: this.editGoal.due_date.substring(0, 10),
+      };
+    }
+  }
 
   handleSubmit() {
     this.onSubmit.emit({
