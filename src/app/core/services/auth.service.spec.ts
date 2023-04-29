4a313df8b5fb2dfe1352f7c55b7243df2e7a4da4
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-04-29 12:03:42 -0400

    refactor, add new spects for auth test

--- src/app/core/services/auth.service.spec.ts
+++ src/app/core/services/auth.service.spec.ts
@@ -20,12 +20,11 @@ describe('AuthService', () => {
     localStorageProto = Object.getPrototypeOf(localStorage);
 
     // spy on local storage
-
     spyOn(localStorageProto, 'setItem');
     spyOn(localStorageProto, 'removeItem');
   });
 
-  it('should be createdd', () => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
@@ -50,7 +49,7 @@ describe('AuthService', () => {
     expect(localStorageProto.removeItem).toHaveBeenCalledWith('user');
   });
 
-  it('should get null from local storage when no user is set', () => {
+  it('should get null from get local user when no user is set', () => {
     spyOn(localStorageProto, 'getItem').and.returnValue(null);
     const user = service.getUserFromLocalStorage();
     expect(user).toBeNull();
@@ -66,10 +65,43 @@ describe('AuthService', () => {
   });
 
   it('should get token from user subject', () => {
-    const loggedInUser = { username: 'test', token: 'test' };
-    service.login({ username: 'test', password: 'test' }).subscribe();
+    // arrange
+    const access_token = 'test_token';
+    const loggedInUser = { username: 'test', token: access_token };
+    spyOn(service, 'getCurrentToken').and.returnValue(loggedInUser.token);
 
+    // act (mainly)
+    service.login({ username: 'test', password: 'test' }).subscribe();
+    httpTestingController
+      .expectOne(service.apiUrl + '/token')
+      .flush({ access_token });
     const token = service.getCurrentToken();
+
+    // assert
     expect(token).toEqual(loggedInUser.token);
+  });
+
+  it('should return true when token is expired', () => {
+    const pastDate = new Date('2022-04-01T00:00:00');
+
+    const expiredToken = `header.${btoa(
+      JSON.stringify({
+        sub: 'test',
+        exp: Math.floor(pastDate.getTime() / 1000) + 60 * 60,
+      })
+    )}.secret`;
+
+    expect(service.tokenExpired(expiredToken)).toBe(true);
+  });
+
+  it('should return false when token is not expired', () => {
+    const validToken = `header.${btoa(
+      JSON.stringify({
+        sub: 'test',
+        exp: Math.floor(Date.now() / 1000) + 60 * 60,
+      })
+    )}.secret`;
+
+    expect(service.tokenExpired(validToken)).toBe(false);
   });
 });
